6f8ed2ecf30a92b3c003747620e26379027bbf01
marijusiMac <marijuslau@yahoo.com>
2021-03-02 21:00:47 +0200

    add statistics ui and link vc

---
+++ budget-lite/Controller/StatisticsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  StatisticsViewController.swift
+//  budget-lite
+//
+//  Created by Marijus Laucevicius on 2021-03-02.
+//
+
+import UIKit
+
+class StatisticsViewController: UIViewController {
+    @IBOutlet weak var expensesTypeLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var categoriesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}

88258f9fab533e0cf964c2168633bc0a35493bf2
marijusiMac <marijuslau@yahoo.com>
2021-03-03 19:53:47 +0200

    display uipicker with years and months

--- budget-lite/Controller/StatisticsViewController.swift
+++ budget-lite/Controller/StatisticsViewController.swift
@@ -12,13 +12,28 @@ class StatisticsViewController: UIViewController {
     @IBOutlet weak var expensesTypeLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var categoriesTableView: UITableView!
+    @IBOutlet weak var periodTextField: UITextField!
+    
+    var periodPicker = UIPickerView()
     
     let currentYear = Date().get(.year)
     let currentMonth = Date().get(.month)
     let currentDay = Date().get(.day)
     
+    var existingDates: [Int : [Int]] = [:]
+    var years: [Int] = []
+    var months: [Int] = []
+    
+    var selectedYear: Int?
+    var selectedMonth: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        periodTextField.inputView = periodPicker
+        
+        periodPicker.dataSource = self
+        periodPicker.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,5 +52,53 @@ class StatisticsViewController: UIViewController {
     
         totalLabel.text = "-" + String(format: "%.2f", total) + "€"
     }
+}
 
+extension StatisticsViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        for date in RealmService.shared.spendingDates! {
+            
+            if !years.contains(date.year) {
+                years.append(date.year)
+            }
+            
+            if !months.contains(date.month) {
+                months.append(date.month)
+            }
+        }
+        
+        
+        if component == 0 {
+            return years.count
+        } else {
+            return months.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return String(years[row])
+        } else {
+            return String(months[row])
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 {
+            selectedYear = years[row]
+            pickerView.reloadComponent(1)
+        } else {
+            selectedMonth = months[row]
+        }
+        
+        if let year = selectedYear, let month = selectedMonth {
+            periodTextField.text = String(year) + "-" + String(month)
+            periodTextField.resignFirstResponder()
+        }
+    }
+    
 }

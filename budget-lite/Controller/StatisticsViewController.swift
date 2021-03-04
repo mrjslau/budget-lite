a032ba52286df5be7398814a65bbce69afc6f422
marijusiMac <marijuslau@yahoo.com>
2021-03-04 14:14:09 +0200

    add functionality to select period and show total expenses for that period

--- budget-lite/Controller/StatisticsViewController.swift
+++ budget-lite/Controller/StatisticsViewController.swift
@@ -19,21 +19,44 @@ class StatisticsViewController: UIViewController {
     let currentYear = Date().get(.year)
     let currentMonth = Date().get(.month)
     let currentDay = Date().get(.day)
-    
-    var existingDates: [Int : [Int]] = [:]
+
     var years: [Int] = []
-    var months: [Int] = []
+    var months: [String] = Calendar.current.shortMonthSymbols
     
-    var selectedYear: Int?
-    var selectedMonth: Int?
+    var selectedYear: Int? = Date().get(.year)
+    var selectedMonth: Int? = Date().get(.month)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         periodTextField.inputView = periodPicker
         
+        
         periodPicker.dataSource = self
         periodPicker.delegate = self
+        
+        
+        
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor(red: 76/255, green: 217/255, blue: 100/255, alpha: 1)
+        toolBar.sizeToFit()
+
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: #selector(self.donePicker))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.donePicker))
+
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        periodTextField.inputAccessoryView = toolBar
+    }
+    
+    @objc func donePicker() {
+        updateTotalLabel()
+        periodTextField.resignFirstResponder()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -41,7 +64,7 @@ class StatisticsViewController: UIViewController {
     }
     
     func updateTotalLabel() {
-        let dates = RealmService.shared.spendingDates?.filter("year == %@ AND month == %@", currentYear, currentMonth)
+        let dates = RealmService.shared.spendingDates?.filter("year == %@ AND month == %@", selectedYear, selectedMonth)
         var total = 0.0
         
         for date in dates! {
@@ -61,16 +84,10 @@ extension StatisticsViewController: UIPickerViewDataSource, UIPickerViewDelegate
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         for date in RealmService.shared.spendingDates! {
-            
             if !years.contains(date.year) {
                 years.append(date.year)
             }
-            
-            if !months.contains(date.month) {
-                months.append(date.month)
-            }
         }
-        
         
         if component == 0 {
             return years.count
@@ -83,7 +100,7 @@ extension StatisticsViewController: UIPickerViewDataSource, UIPickerViewDelegate
         if component == 0 {
             return String(years[row])
         } else {
-            return String(months[row])
+            return months[row]
         }
     }
     
@@ -92,13 +109,16 @@ extension StatisticsViewController: UIPickerViewDataSource, UIPickerViewDelegate
             selectedYear = years[row]
             pickerView.reloadComponent(1)
         } else {
-            selectedMonth = months[row]
+            selectedMonth = row + 1
         }
         
-        if let year = selectedYear, let month = selectedMonth {
-            periodTextField.text = String(year) + "-" + String(month)
-            periodTextField.resignFirstResponder()
-        }
+        //if let year = selectedYear, let _ = selectedMonth {
+        //    periodTextField.text = String(year) + "-" + months[row]
+        //    periodTextField.resignFirstResponder()
+        //
+        //
+        //    updateTotalLabel()
+        //}
     }
     
 }

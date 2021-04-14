71e95af7cf9104118a2550eb5ac1cff08a73a2bf
Marijus <marijuslau@yahoo.com>
2021-04-14 12:17:48 +0200

    add currency cell files

---
+++ budget-lite/Custom Elements/Cells/CurrencyCell.swift
@@ -0,0 +1,15 @@
+//
+//  CurrencyCell.swift
+//  budget-lite
+//
+//  Created by Marijus Laucevicius on 2021-04-14.
+//
+
+import UIKit
+
+class CurrencyCell: UITableViewCell {
+    @IBOutlet weak var codeLabel: UILabel!
+    @IBOutlet weak var flagLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var amountTextField: UITextField!
+}
